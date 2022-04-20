3e0616df20a8e9b98c20cf84b6ee650f36742842
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2022-04-19 23:47:43 -0400

    Add parameters to ExpressionParameters

--- Assets/VRCFaceTracking/Tools/Binary Parameter Tool/Editor/ParameterGenerator.cs
+++ Assets/VRCFaceTracking/Tools/Binary Parameter Tool/Editor/ParameterGenerator.cs
@@ -1,12 +1,50 @@
-﻿using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace VRCFaceTracking.EditorTools
 {
     public class ParameterTools
     {
+        public static bool AddVRCParameter(VRCAvatarDescriptor avatarDescriptor, List<VRCExpressionParameters.Parameter> parameters)
+        {
+            // Make sure Parameters aren't null
+            if (avatarDescriptor.expressionParameters == null)
+            {
+                Debug.LogWarning("ExpressionsParameters not found!");
+                return false;
+            }
+            // Instantiate and Save to Database
+            VRCExpressionParameters newParameters = avatarDescriptor.expressionParameters;
+            string assetPath = AssetDatabase.GetAssetPath(avatarDescriptor.expressionParameters);
+            if (assetPath != String.Empty)
+            {
+                AssetDatabase.RemoveObjectFromAsset(avatarDescriptor.expressionParameters);
+                AssetDatabase.CreateAsset(newParameters, assetPath);
+                avatarDescriptor.expressionParameters = newParameters;
+            }
+
+            foreach (VRCExpressionParameters.Parameter parameter in parameters)
+            {
+                // Make sure the parameter doesn't already exist
+                VRCExpressionParameters.Parameter foundParameter = newParameters.FindParameter(parameter.name);
+                if (foundParameter == null || foundParameter.valueType != parameter.valueType)
+                {
+                    // Add the parameter
+                    List<VRCExpressionParameters.Parameter> betterParametersBecauseItsAListInstead =
+                        newParameters.parameters.ToList();
+                    betterParametersBecauseItsAListInstead.Add(parameter);
+                    newParameters.parameters = betterParametersBecauseItsAListInstead.ToArray();
+                }
+            }
+            return true;
+        }
+        
         public static void CheckAndCreateParameter(string name, AnimatorController animatorController, int type)
         {
             AnimatorControllerParameterType _typeEnum = (AnimatorControllerParameterType)type;
